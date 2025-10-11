130ca8c999475077f141c231857e8c92fbade092
ThomasDixini <thomasdixini.dev@gmail.com>
2025-10-10 23:02:30 -0300

    feat: :sparkles: Pagina de contato

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
 export class Contact {
-
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
 }
