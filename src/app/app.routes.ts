3c97cabbcf29c6de0db0c11f5b93d1814b9f2e7f
ThomasDixini <thomasdixini.dev@gmail.com>
2025-10-10 22:00:04 -0300

    feat: :sparkles: Adicionando rotas na aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Projects } from './projects/projects';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '', 
+        title: 'Sobre Mim',
+        component: About
+    },
+    {
+        path : 'projects', 
+        title: 'Projetos',
+        component: Projects
+    },
+    {
+        path : 'projects/:id', 
+        title: 'Projeto',
+        component: Projects
+    },
+    {
+        path : 'contact', 
+        title: 'Contato',
+        component: Contact
+    },
+];
