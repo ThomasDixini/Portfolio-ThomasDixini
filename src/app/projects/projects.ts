390e6a48166b64cc804af18ac008e825b7549c0e
ThomasDixini <thomasdixini.dev@gmail.com>
2025-10-10 22:34:13 -0300

    feat: :sparkles: Pagina de projetos

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss'
 })
 export class Projects {
-
+  public products = [
+    {
+      name: 'Produto 1',
+      image: '',
+      price: '0.99'
+    },
+    {
+      name: 'Produto 1',
+      image: '',
+      price: '0.99'
+    },
+    {
+      name: 'Produto 1',
+      image: '',
+      price: '0.99'
+    },
+    {
+      name: 'Produto 1',
+      image: '',
+      price: '0.99'
+    },
+    {
+      name: 'Produto 1',
+      image: '',
+      price: '0.99'
+    },
+    {
+      name: 'Produto 1',
+      image: '',
+      price: '0.99'
+    },
+  ];
 }
